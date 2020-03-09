eacb2e5e646c7828c89dba3e7e5e75480bab5ce5
polyakamorph <polyakamorph@gmail.com>
2020-03-09 13:29:16 +0300

    Task 3.2

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -22,4 +23,13 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id       INTEGER REFERENCES users(id),
+  description   VARCHAR NOT NULL,
+  calories      INTEGER NOT NULL,
+  datetime      TIMESTAMP DEFAULT now() NOT NULL
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,18 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (id, name, email, password) VALUES
+  (100000,'User', 'user@yandex.ru', 'password'),
+  (100001, 'Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(id,user_id, datetime,description,calories) VALUES
+  (100002,100000,'2020-03-09 10:00:00','Завтрак','800'),
+  (100003,100000,'2020-03-09 15:00:00','Обед','1500'),
+  (100004,100000,'2020-03-09 20:00:00','Ужин','500'),
+  (100005,100001,'2020-03-09 14:00:00','Шаверма смерти','2500');
